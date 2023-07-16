effc426c39cabd407b40b3b8e37e08ec7b796558
BouzziteLahcen <lbouzzite03@gmail.com>
2023-07-16 17:04:38 +0100

    Connect Angular and Bootstrap

--- back-end/student/src/main/java/com/spring/student/model/Student.java
+++ back-end/student/src/main/java/com/spring/student/model/Student.java
@@ -47,4 +47,98 @@ public class Student {
     @Column(name = "dateUpdated")
     private Date dateUpdated;
 
+    public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(Long id, String fullName, Gender gender, String phone, String age, String address, Date dateCreated,
+			Date dateUpdated) {
+		super();
+		this.id = id;
+		this.fullName = fullName;
+		this.gender = gender;
+		this.phone = phone;
+		this.age = age;
+		this.address = address;
+		this.dateCreated = dateCreated;
+		this.dateUpdated = dateUpdated;
+	}
+
+	public Student(String fullName, Gender gender, String phone, String age, String address, Date dateCreated,
+			Date dateUpdated) {
+		super();
+		this.fullName = fullName;
+		this.gender = gender;
+		this.phone = phone;
+		this.age = age;
+		this.address = address;
+		this.dateCreated = dateCreated;
+		this.dateUpdated = dateUpdated;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Date getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(Date dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+    
+    
 }
