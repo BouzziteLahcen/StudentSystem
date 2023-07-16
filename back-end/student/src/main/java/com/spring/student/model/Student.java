4b9ce8aa61d2863874a0f075c33ab3bd3e34ec93
BouzziteLahcen <lbouzzite03@gmail.com>
2023-07-16 16:12:19 +0100

    set Configuration of database

--- back-end/student/src/main/java/com/spring/student/model/Student.java
+++ back-end/student/src/main/java/com/spring/student/model/Student.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,6 +26,7 @@ public class Student {
 	@Column(name = "fullName")
     private String fullName;
 
+	@Enumerated(EnumType.STRING)
 	@Column(name = "gender")
     private Gender gender;
 
