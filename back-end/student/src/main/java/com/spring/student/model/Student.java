57116d611d811b6d6162faf495c4fd9de03956ae
BouzziteLahcen <lbouzzite03@gmail.com>
2023-07-16 16:06:30 +0100

    add Annotation in  Model student

--- back-end/student/src/main/java/com/spring/student/model/Student.java
+++ back-end/student/src/main/java/com/spring/student/model/Student.java
@@ -2,10 +2,14 @@ package com.spring.student.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.spring.student.enums.Gender;
 
@@ -14,20 +18,30 @@ public class Student {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
     private Long id;
-
+ 
+	@Column(name = "fullName")
     private String fullName;
 
+	@Column(name = "gender")
     private Gender gender;
 
+	@Column(name = "phone")
     private String phone;
 
+	@Column(name = "age")
     private String age;
 
+	@Column(name = "address")
     private String address;
 
+    @CreationTimestamp
+    @Column(name = "dateCreated")
     private Date dateCreated;
 
+    @UpdateTimestamp
+    @Column(name = "dateUpdated")
     private Date dateUpdated;
 
 }
