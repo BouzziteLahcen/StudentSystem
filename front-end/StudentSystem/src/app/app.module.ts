a6b3738953fca6121ad152949ddd61f3367c4b8c
BouzziteLahcen <lbouzzite03@gmail.com>
2023-07-16 18:26:33 +0100

    Components Angular

--- front-end/StudentSystem/src/app/app.module.ts
+++ front-end/StudentSystem/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// myComponents 
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+	LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +24,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
